a99842e93b4d3606cb0be4b76ff5dcee7ca1fd60
Jasper Post <jasper.post@cloudnation.nl>
2026-09-03 11:39:41 +0200

    Remove moved block

--- moved_v4_to_v5.tf
+++
@@ -1,6 +0,0 @@
-# covers the "lt1" example key; other keys need:
-#   terraform state mv 'module.<name>.azurerm_load_test.this["<key>"]' 'module.<name>.azurerm_load_test.this'
-moved {
-  from = azurerm_load_test.this["lt1"]
-  to   = azurerm_load_test.this
-}
